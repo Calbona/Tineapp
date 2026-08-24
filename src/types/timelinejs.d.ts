56992c9d448721ab628df6a65a966355e3ba6590
Calbona <13651980026@163.com>
2026-08-24 20:50:28 +0800

    chore: remove v0.1.0 legacy files

--- src/types/timelinejs.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@knight-lab/timelinejs' {
-	export class Timeline {
-		constructor(
-			containerId: string,
-			data: Record<string, unknown>,
-			options?: Record<string, unknown>,
-		);
-	}
-}
